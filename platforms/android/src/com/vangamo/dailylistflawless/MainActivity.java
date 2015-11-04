3ef5da9ab4ed446107e0bf2ab37e7563b4090696
van Gamo <vangamo.beta@gmail.com>
2015-11-04 18:52:04 +0100

    Changes to implement a different apk name.

--- platforms/android/src/com/vangamo/dailylistflawless/MainActivity.java
+++ platforms/android/src/com/vangamo/dailylistflawless/MainActivity.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.vangamo.dailylist;
+package com.vangamo.dailylistflawless;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
--- platforms/android/src/com/vangamo/dailylistflawless/MyListActivity.java
+++ platforms/android/src/com/vangamo/dailylistflawless/MyListActivity.java
@@ -1,4 +1,4 @@
-package com.vangamo.dailylist;
+package com.vangamo.dailylistflawless;
 
 import android.app.ListActivity;
 import android.content.Intent;
